9e96c505714f7b31d01101cff0620ef8e2940ec7
Eduardo Moraes <edumoraes@live.com>
2021-03-06 10:30:10 -0300

    new files

---
+++ stringops.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 10:05:56 2021
+
+@author: EduardoWork
+"""
+
+hi = 'Hello'
+print(hi[2])
+print('Hello'[2])
+if hi[2] == hi[3]:
+    print('double l')
+
+greeting = 'hi ' + 'how ' + 'are you?'
+print(greeting)
+
+print('bye' * 2)
+
+print('1ellooo' > 'hello')
---
+++ stringslices.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 10:18:49 2021
+
+@author: EduardoWork
+"""
+
+s = 'Hello, how are you guys?'
+
+print(s[2:4] + ' ok')
+print(s[:6] + ' my name is Slim Shady')
+print(s[15:-1] + ' are crazy')
+
+s = '0123456789'
+
+print(s[2:7])
+print(s[2:7:3])
+print(s[8:2:-1])
+print(s[::-3])
